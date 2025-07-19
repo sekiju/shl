e7872da01bbb4d4af0ed36bb35bc5babe3c0e92b
sekiju <zzzcxk@gmail.com>
2025-07-20 02:17:14 +0500

    feat: utoipa for shl-ntex

--- shl-ntex/src/error.rs
+++ shl-ntex/src/error.rs
@@ -1,8 +1,10 @@
 pub use ntex_error_macro::*;
 use serde::Serialize;
 use std::collections::HashMap;
+use utoipa::ToSchema;
 
 #[derive(Debug, Serialize)]
+#[cfg_attr(feature = "utoipa", derive(ToSchema))]
 pub struct NtexErrorResponse {
     pub code: String,
     pub message: String,
